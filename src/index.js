1fa6920e3d4c46925b381315f9b3ad24d1270dc2
DonBrowny <kishore13ask@gmail.com>
2022-06-24 07:31:05 +0530

    added overlay

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+let menuOpen = false;
 function onHamburgerClick(element) {
     element.classList.toggle("open");
+    document.getElementById("overlay").classList.toggle("open");
     const nav = document.getElementsByTagName('nav');
-    console.log(nav)
     nav[0].classList.toggle("open");
+    menuOpen = !menuOpen;
+    const body = document.body;
+    if(menuOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "auto";
+    }
   }
